97c3245ba261683c61341ec3326280b44db1c4b0
Alex Aaron <alexfrancisaaron@gmail.com>
2025-01-02 14:16:09 -0600

    added instructions to hof.js

--- week-2/day-1/higher-order-functions/hof.js
+++ week-2/day-1/higher-order-functions/hof.js
@@ -1,33 +1,57 @@
-// find()
-const find = (array, func) => {
-    for (let i = 0; i < array.length; i++){
-        if (func(array[i], i, array)){
-            return array[i];
-        }
-    }
+
+
+/** find()
+ * Arguments:
+ *  1) Array
+ *  2) Function
+ * Objectives:
+ *  1) call <function> on each element in <array> passing the argments:
+ *     element, index, and <array>.
+ *  2) return the item in the array for which calling <function> returned true or truthy
+ *  3) return undefined if no item returns true or truthy
+ * Examples:
+ *  find(['Alex Aaron', 'Stephanie Cooper', 'Bethany Jones'], function(name){ return name === 'Stephanie Cooper'}); // => 'Stephanie Cooper'
+ */
+
+const find = () => {
+    
 };
 
-// mapByDataType()
-const mapByDataType = (array, func, datatype) => {
-    const output = [];
-    for (let i = 0; i < array.length; i++){
-        if (typeof array[i] === datatype){
-            const result = func(array[i], i, array);
-            output.push(result);
-        }
-    }
-    return output;
+/** mapByDataType()
+ * Arguments:
+ *  1) Array
+ *  2) Function
+ *  3) String of a datatype
+ * Objectives: 
+ *  1) iterate through the input array and at each iteration determine if the current element matches the input <datatype>
+ *  2) call <function> on each element whose datatype matches <datatype>
+ *  3) save the return value of each function call in a new array and return it at the end
+ * Example:
+ *  mapByDataType([null, 1, 'a', 2], function(item){ return item * 10 }, 'number'); // => [10, 20]
+ */
+
+
+const mapByDataType = () => {
+   
 };
 
-// filterByCondition
-const filterByCondition = (array, test, condition) => {
-    const output = [];
-    for (let i = 0; i < array.length; i++){
-        if (condition(i, array)){
-            if (test(array[i], i, array)){
-                output.push(array[i]);
-            }
-        }
-    }
-    return output;
-}
+/** filterByCondition()
+ * Arguments:
+ *  1) Array
+ *  2) Function to test each item
+ *  3) Function to test each index
+ * Objectives:
+ *  1) iterate through the input array and invoke <conditiion> at each iteration passing in the arguments: 
+ *     current index, <array>
+ *  2) if the result of invoking <condition> is true or truthy, invoke <test> on each item passing in the arguments:
+ *     current item, current index, <array>
+ *  3) return a new array of elements for which calling <test> was true or truthy
+ * Example:
+ *  filterByCondition([10, 20, 30, 40], (n) => n >= 30, (index, array) => {
+ *   return index > 1
+ *  }); // => [30, 40];
+ * 
+ */
+const filterByCondition = () => {
+    
+};
