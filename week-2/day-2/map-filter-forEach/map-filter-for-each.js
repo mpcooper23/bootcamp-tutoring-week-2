0860ff9517079b7a2ea9603cb0fe41bd03901699
mpcooper23 <mpcooper23@gmail.com>
2025-01-07 14:04:25 -0600

    4

--- week-2/day-2/map-filter-forEach/map-filter-for-each.js
+++ week-2/day-2/map-filter-forEach/map-filter-for-each.js
@@ -9,9 +9,16 @@ const logCourses = (students) => {
     logCourses => {students.forEach().courses.forEach(console.log(`${courses.past.phase} - ${courses.past.date}`))}
 };
 
-// Problem #3 //
-const filterByCoursesAndLocation = () => {
-    
+// Problem #3 
+//**Create a function called `filterByLocationAndCourses` that takes in two parameters - `array, location` - 
+//`array` represents an array of student objects like the array featured in data.js; `location` represents 
+//a string of a student's location. This function should use the native filter method to return a new array 
+//of only the students whose location property matches the input location and who have at least 1 past course they have completed
+//
+const filterByCoursesAndLocation = (array, location) => {
+    array.filter(s => {if(s === location){
+        return array.push(s)
+    }})
 };
 
 // Problem #4 //
