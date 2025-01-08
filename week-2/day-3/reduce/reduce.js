0207bf4643887146bd3615109eff8193d376be65
mpcooper23 <mpcooper23@gmail.com>
2025-01-08 13:33:03 -0600

    1

--- week-2/day-3/reduce/reduce.js
+++ week-2/day-3/reduce/reduce.js
@@ -1,28 +1,33 @@
 // Problem #1 // 
+
+/**
+ * 
+ */
 const currentBootcampers = () => {
     
 };
 
 // Problem #2 //
+/**
+ * 
+ */
 const getNamesByLocation = () => {
    
 };
 
 // Problem #3 //
+/**
+ * 
+ */
 const getPrecourseNames = () => {
     
 };
 
 // Problem #4 //
-const locationBreakdown = (array) => {
-    return array.reduce((acc, current) => {
-        if (acc[current.location]){
-            acc[current.location] += 1;
-        } else {
-            acc[current.location] = 1;
-        }
-        return acc;
-    }, {});
-};
 
-console.log(locationBreakdown(students));
+/**
+ * 
+ */
+const locationBreakdown = (array) => {
+}
+
