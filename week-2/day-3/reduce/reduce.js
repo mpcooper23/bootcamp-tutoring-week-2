684c72c6230db6ad0c311d461c6d771ca6396a2f
mpcooper23 <mpcooper23@gmail.com>
2025-01-08 14:01:50 -0600

    2

--- week-2/day-3/reduce/reduce.js
+++ week-2/day-3/reduce/reduce.js
@@ -1,23 +1,36 @@
 // Problem #1 // 
 
 /**
- * 
+ * Create a function called `currentBootcampers` that takes in one parameter - `array` - 
+ * which represents an array of student objects like the one featured in data.js. This 
+ * function should use the native reduce method to return the number of students who are 
+ * currently in Bootcamp.
  */
-const currentBootcampers = () => {
-    
+const currentBootcampers = (array) => {
+    return array.reduce((acc, student) => {
+        if(student.courses.phase === "Bootcamp"){
+        return acc + 1;
+    }
+    return acc;}, 0);
 };
 
 // Problem #2 //
 /**
- * 
+ * Create a function called `getNamesByLocation` that takes in two parameters - `array, location` 
+ * - `array` represents an array of student objects like the one featured in data.js; `location` 
+ * represents a string of a student's location. This function should return an array of the student's 
+ * names who live in the input location.
  */
-const getNamesByLocation = () => {
+const getNamesByLocation = (array, location) => {
    
 };
 
 // Problem #3 //
 /**
- * 
+ * Create a function called `getPrecourseNames` that takes in one parameter - `array` - which represents 
+ * an array of student objects like the one featured in data.js. This function should use the native 
+ * reduce method to return a string of every student's name who has completed Precourse followed by 
+ * linebreak characters.
  */
 const getPrecourseNames = () => {
     
@@ -26,8 +39,12 @@ const getPrecourseNames = () => {
 // Problem #4 //
 
 /**
- * 
+ * Create a function called `locationBreakdown` that takes in one parameter - `array` - which 
+ * represents an array of student objects like the one featured in data.js. This function should 
+ * use the native reduce method to return an object where each key is a location and the value at 
+ * the key is the number of students from that location.
  */
-const locationBreakdown = (array) => {
-}
+const locationBreakdown = () => {
+
+};
 
