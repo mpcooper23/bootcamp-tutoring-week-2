4cb9c105e17e4acbc327b92c5f11650aab45585a
mpcooper23 <mpcooper23@gmail.com>
2025-03-12 18:31:53 -0500

    6

--- week-2/day-2/map-filter-forEach/map-filter-for-each.js
+++ week-2/day-2/map-filter-forEach/map-filter-for-each.js
@@ -33,18 +33,17 @@ const logCourses = (students) => {
 //of only the students whose location property matches the input location and who have at least 1 past course they have completed
 //
 
+
 let filterByCoursesAndLocation = (array, location) => {
-  array.filter(s => {
-    for(let i = 0; i < s.courses.length; i++){
-      if(s.courses[i].past){
-        if(s.location === location){
-          return true;
-        }
-      }
+ return array.filter(s => {
+    if(s.courses.past && s.courses.past.length > 0){
+      return s.location === location
     }
     return false;
   })
 }
+  
+
 
 // Problem #4 //
 //**Create a function called `filterByBootcamp` that takes in one parameter - `array` - 
@@ -52,14 +51,27 @@ let filterByCoursesAndLocation = (array, location) => {
 //function should use the native filter method to return a new array of only the students 
 //who have completed Bootcamp (if their current phase is Bootcamp, they have not completed the course yet).
  
-
+let filterByBootcamp = (array) => {
+  return array.filter(s => {
+for(let i = 0; i < s.courses.past.length; i++){
+  if(s.courses.past[i].phase === 'Bootcamp'){
+    return true;
+  }
+}
+})
+}
 // Problem #5 //
 /**
  * Create a function called `mapCurrentCourses` that takes in one parameter - `array` - 
  * which represents an array of student objects like the array featured in data.js. 
  * This function should use the native map method to return a new array of strings of 
  * all every student's current course name and date as shown in the example below.
+ * 
  */
+
+let mapCurrentCourses = array => {
+  return array.map(s => `${s.courses.current.phase} - ${s.courses.current.date}`)
+}
 
 // Problem #6 //
 /*Create a function called `mapPastCourses` that takes in one parameter - `array` - 
